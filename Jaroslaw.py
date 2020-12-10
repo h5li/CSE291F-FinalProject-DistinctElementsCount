78e52c8745fa4004f872815b9eb0ebdb62592996
Han Li <h5li@ucsd.edu>
2020-12-10 14:54:31 -0800

    Update algorithms and test

--- Jaroslaw.py
+++ Jaroslaw.py
@@ -1,16 +1,23 @@
 import numpy as np
+import math
+import random
+from JohnnyChen import findPrimeNumber
+import gmpy
 def count(elements, n):
     if len(elements) == 0:
         return 0
-    num_bits = int(np.log(n))
     max_bit = -1
+    p = findPrimeNumber(n,2*n)
+    a = random.randint(1,p-1)
+    b = random.randint(1,p-1)
+
     for e in elements:
-        remainder = e % num_bits
-        max_bit = max(max_bit, remainder)
+        remainder = ((a*e + b) % p) % n
+        max_bit = max(max_bit, gmpy.scan1(int(remainder)))
     return 2 ** max_bit
 def count_advanced(elements, n, delta):
     result = []
-    for i in range(int(np.log(1/delta))):
+    for i in range(int(np.log2(1/delta))):
         result.append(count(elements, n))
     return np.median(result)
 
